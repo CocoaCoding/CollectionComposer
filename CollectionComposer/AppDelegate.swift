530b997c7f0815f45d2d775d6954677a25e2ad39
Holger Hinzberg <holger@hinzberg.de>
2019-06-30 22:05:33 +0200

    Initial Commit

---
+++ CollectionComposer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollectionComposer
+//
+//  Created by Holger Hinzberg on 30.06.19.
+//  Copyright © 2019 Holger Hinzberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
